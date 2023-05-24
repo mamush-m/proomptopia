035d69e95cafef4c19772476a218f25d063fc12f
mamush-m <svrmamush@gmail.com>
2023-05-24 01:42:01 -0400

    sign-in

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,8 +7,64 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
+    const isUserLoggedIn = true;
+
+    const [providers, setProviders] = useState(null);
+    useEffect(() => {
+        (async () => {
+            const res = await getProviders();
+            setProviders(res);
+        })();
+    }, [])
   return (
-    <div>Nav</div>
+    <nav className="flex-between w-full mb-16 pt-3">
+        <Link href='/' className="flex gap-2 flex-center">
+            <Image 
+                src='./assets/images/logo.svg'
+                alt="Proomptopia Logo"
+                width={30}
+                height={30}
+                className="object-contain"
+            />
+            <p className="logo_text">Proomptopia</p>
+        </Link>
+
+        {/* Mobile Navigation */}
+        <div className="sm:flex hidden">
+            {
+                isUserLoggedIn ? (
+                    <div className="flex gap-3 md:gap-5">
+                        <Link href='/create-prompts' className="black_btn">Create Post</Link>
+
+                        <button type="button" onClick={signOut} className="outline_btn">Sign Out</button>
+
+                        <Link href='/profile'>
+                            <Image
+                                src='/assets/images/logo.svg'
+                                width={37}
+                                height={37}
+                                className="rounded-full"
+                                alt="profile"
+                            >
+
+                            </Image>
+                        </Link>
+                    </div>
+                ) : (<>
+                    {providers && Object.values(providers).map(provider => {
+                        return (<button
+                            type="button"
+                            key={provider.name}
+                            onClick={() => signIn(provider.id)}
+                            className="black_btn"
+                        >
+                            Sign In
+                        </button>)
+                    })}
+                </>)
+            }
+        </div>
+    </nav>
   )
 }
 
