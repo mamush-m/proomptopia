747d9ad8dd01c33b4412098c1b6926c2c73f5499
mamush-m <svrmamush@gmail.com>
2023-05-23 22:07:49 -0400

    nav bar added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import '@styles/global.css';
 
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
 export const metadata = {
     title: 'Proomptopia',
     description: 'Discover & Share AI Prompts'
@@ -8,12 +11,13 @@ export const metadata = {
 const RootLayout = ({children}) => {
   return (
     <html lang='en'>
-        <body className='bg-green-400'>
+        <body className=''>
             <div className="main">
                 <div className="gradient"/>
             </div>
 
             <main className="app">
+                <Nav/>
                 {children}
             </main>
         </body>
