4d06f53c27df614134253f7710afd9d383a51f30
mamush-m <svrmamush@gmail.com>
2023-05-30 22:21:34 -0400

    prompts layout

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+'use client';
+
+import { useState, useEffect } from 'react';
+
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {
+        data.map(post => {
+          return(
+            <PromptCard
+              key={post._id}
+              post={post}
+          />
+          ) 
+        })
+      }
+    </div>
+  )
+}
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([])
+
+  const handleSearchChange = e => {
+
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+      console.log('lala land', data);
+      
+      setPosts(data);
+    }
+
+    // console.log('ALL POSTS', posts);
+    fetchPosts();
+    console.log('ALL POSTSsssss', posts);
+  }, [])
+
   return (
-    <div>Feed</div>
+    <section className='feed'>
+      <form className='relative w-full flex flex-center' action="">
+        <input
+          type="text"
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+
+      <PromptCardList
+        data={posts}
+        handleTagClick={() => {}}
+      />
+    </section>
   )
 }
 
--- utils/database.js
+++ utils/database.js
@@ -14,16 +14,16 @@ export const connectToDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
             dbName: 'share_prompt',
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         })
 
         isConnected = true;
 
         console.log('MongoDB connected');
     } catch (error) {
-        console.log(error);
+        console.log('DB ERROR logger', error);
     }
 }
 
-console.log(process.env);
+// console.log('PROCESS.ENV logger', process.env);
